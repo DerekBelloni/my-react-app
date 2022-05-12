5100ac7fd6795ef0a9ce02da56b6da0d02121ab6
Derek <derekbelloni10@gmail.com>
2022-05-12 09:38:18 -0600

    Card styling changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import reactIcon from '../images/react-icon-small.png'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img className="nav--icon" width="80px" src={reactIcon} alt="" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  )
+}
+
+export default Navbar
