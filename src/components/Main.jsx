b830a6953b54b332aa676faafe0849a33aacb2f0
Derek <derekbelloni10@gmail.com>
2022-05-13 09:39:56 -0600

    added styling changes to list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Main = () => {
   return (
-    <h1>Main Component </h1>
+    <main>
+      <h1 className="main--title">Fun Facts About React!</h1>
+      <ul className="main--facts">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k starts on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
   )
 }
 
